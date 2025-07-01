7a7326c78cdfba06ebe18f79e89a17e9c8c6579a
Anneparvin <anneparvin1@gmail.com>
2025-07-01 17:41:36 +0600

    function

--- 07_day_function/exercise.js
+++ 07_day_function/exercise.js
@@ -81,6 +81,28 @@ const tempC = 30
 const tempF = convertCelsiusToFahrenheit(tempC).toFixed(2)
 console.log(`${tempC}°C is equal to ${tempF}°F`);
 // Body mass index(BMI) is calculated as follows: bmi = weight in Kg / (height x height) in m2. Write a function which calculates bmi. BMI is used to broadly define different weight groups in adults 20 years old or older.Check if a person is underweight, normal, overweight or obese based the information given below.
+function calculateBMI(weightKg, heightM) {
+    const bmi = weightKg / (heightM * heightM);
+    let status = '';
+
+    if (bmi < 18.5) {
+        status = 'Underweight';
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        status = 'Normal weight';
+    } else if (bmi >= 25 && bmi < 29.9) {
+        status = 'Overweight';
+    } else {
+        status = 'Obese';
+    }
+
+    console.log(`Your BMI is ${bmi.toFixed(2)} (${status})`);
+    return bmi;
+}
+
+const weightKg = 100; // define first
+const heightM = 1.75; // define first
+
+calculateBMI(weightKg, heightM); // use after declaration
 
 // The same groups apply to both men and women.
 // Underweight: BMI is less than 18.5
@@ -88,6 +110,26 @@ console.log(`${tempC}°C is equal to ${tempF}°F`);
 // Overweight: BMI is 25 to 29.9
 // Obese: BMI is 30 or more
 // Write a function called checkSeason, it takes a month parameter and returns the season:Autumn, Winter, Spring or Summer.
+function checkSeason(month){
+    month = month.toLowerCase();
+if(["september","october","november"].includes(month)){
+    return "Autumn";
+}else if (["december","january","february"].includes(month)){
+    return "Winter";
+} else if(["march","april","may"].includes(month)){
+return "Spring";
+} else if (["june","july","auguest"].includes(month)){
+        return "Summer";
+}
+ else {
+    return "Invalid Month"
+}
+}
+console.log(checkSeason("March"));     // Spring
+console.log(checkSeason("july"));      // Summer
+console.log(checkSeason("NOVEMBER"));  // Autumn
+console.log(checkSeason("January"));   // Winter
+console.log(checkSeason("abc"));       // Invalid month
 
 // Math.max returns its largest argument. Write a function findMax that takes three arguments and returns their maximum with out using Math.max method.
 
